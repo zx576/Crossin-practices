9fe47ba4b1130169495a75383232f74bb6e00707
zx576 <782744680@qq.com>
2016-10-10 16:37:55 +0800

    FIB

---
+++ fib.py
@@ -0,0 +1,22 @@
+#输入数字判断
+def input_num():
+    t = input('>>>')
+    try:
+        t = int(t)
+        if t > 3:
+            pass
+        else:
+            fib()
+    except:
+        fib()
+    return t
+#斐波那契输出
+def fib():
+    print('输入一个大于3的数字')
+    a = 0
+    b = 1
+    n = input_num()
+    for i in range(n):
+        print(b)
+        a,b = b,a+b
+fib()
