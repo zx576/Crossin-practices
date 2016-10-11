939efccda7beb57c52cc570d7e798d9fea618f1f
zx576 <782744680@qq.com>
2016-10-11 16:41:33 +0800

    xiugaiban

--- practice1_guessNum.py
+++ practice1_guessNum.py
@@ -1,61 +1,29 @@
 import random
-setnum = random.randint(0, 100)
-#输入数字并判断
-def inputnum():
-    yournum = input('type in a number:')
-    try:
-        yournum = int(yournum)
-    except:
-        print('输入的不是数字哦')
-        GuessNum()
+
+#输入模块，1.在程序运行过程中任何时候输入exit可退出，2.判断数字
+def input_Num():
+    while True:
+        yournum = input('>>>>>>')
+        if yournum == 'exit':
+            exit_game()
+        else:
+            try:
+                yournum = int(yournum)
+                break
+            except:
+                print('invalid number')
+                pass
     return yournum
-# 猜一次的函数
-def GuessNum():
-    times = 0
-    print('1.选择困难模式，2.选择一般模式')
-    mode = input('>>>>')
-    if mode == '1':
-        print('您自己设定一个小于10的最大次数吧')
-        t = inputnum()
-        print('设定的数字在0-99之间，游戏开始咯')
-        while times < t:
-            times += 1
-            yournum = inputnum()
-            if setnum == yournum:
-                print('right number')
-                break
-            elif setnum > yournum:
-                print('try a bigger number')
-            elif setnum < yournum:
-                print('try a lesser number')
-        print('次数用光了,重新开始吧！')
-        GuessNum()
-    elif mode == '2':
-        print('设定的数字在0-99之间，游戏开始咯')
-        while True:
-            times += 1
-            yournum = inputnum()
-            if setnum == yournum:
-                print('right number')
-                break
-            elif setnum > yournum:
-                print('try a bigger number')
-            elif setnum < yournum:
-                print('try a lesser number')
-    else:
-        GuessNum()
-    return times
-# 记录游戏
-def recode():
-    res = startgame()
-    with open('guess.txt', res) as f:
-        result = str(GuessNum())
-        print('您本次游戏共猜了%s次' % (result))
-        f.write(result)
-        f.write('\n')
-#输出游戏
-def output_recode():
-    recode()
+#记录模块，传入文件打开方式以及猜数字结果以便处理
+def recode(open_ways,times):
+    with open('guess.txt', open_ways) as f:
+        result = str(times)
+        if result:
+            print('您本次游戏共猜了%s次' % (result))
+            f.write(result)
+            f.write('\n')
+#读取recode文件并输入结果
+def output():
     total = 0
     fre = 0
     averge = 0
@@ -64,27 +32,91 @@ def output_recode():
             if time:
                 total += int(time)
                 fre += 1
-    averge = round(total / fre, 2)
+
+    averge = total / fre
 
     print('在游戏中您一共猜了%d次' % (total))
-    print('游戏已经进行了%d轮' % (fre))
-    print('平均每次游戏您要猜%f次' % (averge))
-#退出游戏
+    print('游戏进行了%d轮' % (fre))
+    print('平均每次游戏您要猜%0.2f次' % (averge))
+
+#猜数字游戏主体，传入limit参数，控制猜数字次数
+def Guess_Num(limit):
+    print('the set-number is between 0-99,type in a number')
+    times = 0
+    while times < limit:
+        outcome = input_Num()
+        times += 1
+        if setnum == outcome:
+            print('congratulations,you win')
+            return times
+        elif setnum > outcome:
+            print('%d is too small,try again' %(outcome))
+        elif setnum < outcome:
+            print('%d is too large,try again'%(outcome))
+    print('times up')
+
+
+#模式选择
+def Mode_choose():
+    print('choose a mode to play')
+    print('1.easy,2.hard')
+    while True:
+        outcome = input_Num()
+        if outcome == 1:
+            #10000000等同于无限次
+            return 10000000
+        elif outcome == 2:
+            #有限次猜数字
+            print('input a limit number which is less than 10')
+            limit = input_Num()
+            return limit
+
+#退出游戏模块
 def exit_game():
-    print('欢迎再次启动游戏，再见')
+    print('looking forward to coming back,have fun')
     exit()
-#开始游戏
-def startgame():
-    print('starting game...')
-    print('1.新建游戏，2.继续游戏，3.退出')
-    choice = input('>>>>')
-    if choice == '1':
-        return 'w'
-    elif choice == '2':
-        return 'a'
+#开始游戏，返回文件打开方式
+def start():
+    print('welcome to the game...')
+    print('choose game mode: 1.new game,2.continue game,3.exit game')
+    while True:
+        outcome = input_Num()
+        if outcome == 1:
+            #写 模式打开文件，等同于新游戏
+            return 'w'
+        elif outcome == 2:
+            #添加 模式打开问价，等同于存档模式
+            return 'a'
+        elif outcome == 3:
+            exit_game()
+        print('type in 1 or 2 or 3')
+
+#游戏主体逻辑关系
+def main():
+    global setnum
+    setnum = random.randint(0, 100)
+    #开始游戏
+    outcome =start()
+    #选择模式
+    mode = Mode_choose()
+    #进入游戏
+    result = Guess_Num(mode)
+    #猜对了，返回结果并输出，然后进入下一个循环
+    if result:
+        recode(outcome,result)
+        output()
+    #猜错了，不记录人文件，进入下一个循环
     else:
-        exit_game()
-#启动游戏		
+        pass
+    print('please continue game...')
+
+#游戏循环
 while True:
-    output_recode()
+    main()
+
+
+
+
+
+
 
