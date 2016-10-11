c764f2c1a443e3121269127a995757245eea8179
zx576 <782744680@qq.com>
2016-10-11 10:26:12 +0800

    replace words

---
+++ replace_words.py
@@ -0,0 +1,34 @@
+
+#生成替换词文件
+def generate_file():
+    words = ['a1','b2','cc','de','23']
+    with open('words.txt','w')as f:
+        for i in words:
+            f.write(i)
+            f.write('\n')
+#读取替换词文件，并写入list
+def block():
+    global b_words
+    b_words = []
+    with open('words.txt','r') as l:
+        for i in l.readlines():
+            if i:
+                b_words.append(i.strip())
+    print(b_words)
+#替换关键词
+def replace(word):
+    for i in b_words:
+        #print(i)
+        word = word.replace(i,'*'*len(i))
+    print(word)
+
+generate_file()
+block()
+while True:
+    word = input('>>>>')
+    if word:
+        replace(word)
+    else:
+        exit()
+
+
