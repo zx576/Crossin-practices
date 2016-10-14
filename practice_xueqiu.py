74bf92118becffe7bbad70fcc0aa476e5283e0b1
zx576 <782744680@qq.com>
2016-10-14 17:19:40 +0800

    xueqiu-header_change

--- practice_xueqiu.py
+++ practice_xueqiu.py
@@ -6,14 +6,7 @@ import re
 #request xueqiu.com and download data
 def download_data(url):
     headers_data = {
-    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36',
-    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Connection':'keep-alive',
-    'Host':'xueqiu.com',
-    'Cookie':r's=7512d72g25; xq_a_token=7f757b49bad857c0e0010141ddcfeb2c212c6afe;'
-             r' xq_r_token=ee16af62ae60b52f23869019e7231cd655156d57;'
-             r' Hm_lvt_1db88642e346389874251b5a1eded6e3=1475921087,1475921820,1475976041;'
-             r' Hm_lpvt_1db88642e346389874251b5a1eded6e3=1475976051',
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
     }
     rep = urllib.request.Request(url,headers=headers_data)
     reps = urllib.request.urlopen(rep)
